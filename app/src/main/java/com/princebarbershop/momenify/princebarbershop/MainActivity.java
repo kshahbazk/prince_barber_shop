543c01822ee0d641b2768785f0eb866d05dc7b38
Shahbaz Khan <shahbazkhan@Shahbazs-MacBook-Pro.local>
2014-12-02 00:31:37 -0800

    Initial commit

---
+++ app/src/main/java/com/princebarbershop/momenify/princebarbershop/MainActivity.java
@@ -0,0 +1,62 @@
+package com.princebarbershop.momenify.princebarbershop;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.net.Uri;
+import android.content.Intent;
+import android.view.KeyEvent;
+
+
+public class MainActivity extends Activity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        myWebView = new WebView(this);
+        myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        myWebView.getSettings().setUserAgentString("Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3");
+        myWebView.loadUrl("http://mobile.dudamobile.com/site/princebarbershop");
+
+
+        myWebView.setWebViewClient(new WebViewClient() {
+
+
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        Thread splash = new Thread(){
+
+            public void run(){
+                try {
+                    sleep(5000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(new Intent(getApplicationContext(), Splash.class));
+                    finish();
+                }
+            }
+        };
+        splash.start();
+
+        }
+    }
+
+
---
+++ app/src/main/java/com/princebarbershop/momenify/princebarbershop/Splash.java
@@ -0,0 +1,19 @@
+package com.princebarbershop.momenify.princebarbershop;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+
+
+}
